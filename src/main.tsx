8cbd42e4a9a3cfe1a2d4447b01e4ebbaf85be802
yeyu98 <763525908@qq.com>
2024-05-28 11:09:01 +0800

    feat: 暂时注释StrictMode避免调试时执行两次

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+/*
+ * @Author: yeyu98
+ * @Date: 2024-05-27 17:14:27
+ * @LastEditors: yeyu98
+ * @LastEditTime: 2024-05-28 11:08:31
+ * @FilePath: \react-router-debug\src\main.tsx
+ * @Description: 
+ */
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
